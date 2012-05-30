c16c6db5fc1f6b5f1eb97ad17728596581213d9b
Victor <victor.jolissaint@gmail.com>
2012-05-30 23:51:02 +0200

    Add RFC-compliant email address regex

--- js/filters.js
+++ js/filters.js
@@ -27,7 +27,7 @@ var Filters = {
     error: 'Must be at least 8 characters long and contain at least one uppercase and one lowercase letter and one number or special character.'
   },
   email: {
-    regex: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
+    regex: /^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/,
     error: 'Must be a valid e-mail address. <em>(e.g. user@gmail.com)</em>'
   },
   phone: {
