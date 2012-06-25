5ae07bb2b0b0e4c2642a892c0447132610cecb29
elclanrs <elclanrs@gmail.com>
2012-06-25 18:40:03 -0400

    email regex fix

--- js/filters.js
+++ js/filters.js
@@ -30,7 +30,7 @@ var Filters = {
     error: 'Must be at least 8 characters long and contain at least one uppercase and one lowercase letter and one number or special character.'
   },
   email: {
-    regex: /^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/,
+    regex: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b/,
     error: 'Must be a valid e-mail address. <em>(e.g. user@gmail.com)</em>'
   },
   phone: {
@@ -123,7 +123,7 @@ var Filters = {
     regex: function (input, value) {
       var files = input.input[0].files || [{ name: value }],
           extensions = input.userOptions.data.extension,
-          re = new RegExp('\.'+ extensions.join('|') +'$', 'i'),
+          re = new RegExp('\\.'+ extensions.join('|') +'$', 'i'),
           valid = false
       for (var i = 0, len = files.length; i < len; i++) {
         if (re.test(files[i].name)) valid = true
