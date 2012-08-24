53a7c0b4aaef3329f496228d413559ba69a34a02
andre <andrehajo@gmail.com>
2012-08-24 20:49:13 +0200

    german translation
    add a german translation into the project

---
+++ js/i18n/errors_de.js
@@ -0,0 +1,29 @@
+/**
+ * locale de
+ */
+$.extend($.idealforms.errors, {
+
+  required: 'Dieses Feld ist erforderlich.',
+  number: 'Bitte eine gültige Zahl eingeben.',
+  digits: 'Bitte nur Zahlen eingeben.',
+  name: 'Bitte mindestens 3 Buchstaben/ Zeichen eingeben.',
+  username: 'Der Name muss aus mindestens 4 Zeichen bestehen und darf 32 Zeichen nicht überschreiten. Erlaubt sind Buchstaben, Zahlen, Bindestriche und Punkte.',
+  pass: 'Das Passwort muss mindestens aus 6 Zeichen bestehen. Es muss aus Groß- und Kleinbuchstaben, sowie Zahlen enthalten.',
+  strongpass: 'Das Passwort muss aus mindestens 8 Zeichen bestehen. Das Passwort muss Groß- und Kleinbuchstaben, sowie aus Zahlen und Sonderzeichen enthalten.',
+  email: 'Bitte eine gültige E-Mail Adresse eingeben. <em>(Beispiel: user@gmail.com)</em>',
+  phone: 'Bitte eine gültige Telefonnummer eingeben. <em>(Beispiel: 555-123-4567)</em>',
+  zip: 'Bitte eine gültige PLZ eingeben. <em>(Beispiel: 33245)</em>',
+  url: 'Bitte eine gültige URL eingeben. <em>(Beispiel: www.google.com)</em>',
+  minChar: 'Bitte mindestens <strong>{0}</strong> Zeichen eintragen.',
+  minOption: 'Bitte mindestens <strong>{0}</strong> Option/en wählen.',
+  maxChar: 'Bitte nicht mehr als <strong>{0}</strong> Zeichen eingeben.',
+  maxOption: 'Bitte nicht mehr als <strong>{0}</strong> Option/en wählen.',
+  range: 'Bitte eine Zahl zwischen {0} und {1} eingeben',
+  date: 'Bitte ein gültiges Datum eingeben. <em>(Beispiel: {0})</em>',
+  dob: 'Bitte ein gültiges Geburtsdatum eingeben.',
+  exclude: '"{0}" n\'ist nicht verfügbar.',
+  excludeOption: '{0}',
+  equalto: 'Die Eingabe muss mit gleich <strong>"{0}"</strong> sein.',
+  extension: 'Bitte einen gültigen Dateinamen eingeben. <em>(Beispiel: "{0}")</em>'
+
+})
