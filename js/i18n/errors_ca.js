e121d51c181bcfbe972a183d4889effb19dd27c6
Alvaro Garcia <agarcia@gmaur.com>
2013-04-21 20:20:56 +0200

    added translation for errors in Catalan (errors_ca.js)

---
+++ js/i18n/errors_ca.js
@@ -0,0 +1,31 @@
+/**
+ * locale sp
+ */
+$.extend($.idealforms.errors, {
+
+  required: 'Camp obligatori.',
+  number: 'Ha de ser un número.',
+  digits: 'Només pot contenir xifres.',
+  name: 'Ha de contenir com a mínim 3 caràcters i únicament lletres.',
+  username: 'Ha de contenir entre 4 i 32 caràcters i començar amb una lletra. Es poden fer servir lletres, números, guions i un punt.',
+  pass: 'Ha de contenir un mínim de 6 caràcters i com a mínim un número, una lletra majúscula i una minúscula.',
+  strongpass: 'Ha de contenir un mínim de 8 caràcters i com a mínim una lletra majúscula i una minúscula i un número o caràcter especial.',
+  email: 'Ha de ser una direcció de correu vàlida. <em>(e.g. user@gmail.com)</em>',
+  phone: 'Ha de ser un número de telèfon vàlid als Estats Units d\'Amèrica. <em>(e.g. 555-123-4567)</em>',
+  zip: 'Ha de ser un codi postal vàlid als Estats Units d\'Amèrica. <em>(e.g. 33245 or 33245-0003)</em>',
+  url: 'Ha de ser una direcció web vàlida. <em>(e.g. www.google.com)</em>',
+  minChar: 'Ha de contenir al menys <strong>{0}</strong> caràcters.',
+  minOption: 'Selecciona al menys <strong>{0}</strong> opcions.',
+  maxChar: 'No pot contenir més de <strong>{0}</strong> caràcters.',
+  maxOption: 'No puede haver-hi més de <strong>{0}</strong> opcions seleccionades.',
+  range: 'Ha de ser un número entre {0} i {1}.',
+  date: 'Ha de ser una data vàlida. <em>(e.g. {0})</em>',
+  dob: 'Ha de ser una data de neixement vàlida.',
+  exclude: '"{0}" no està disponible.',
+  excludeOption: '{0}',
+  equalto: 'Ha de ser idèntic a <strong>"{0}"</strong>',
+  extension: 'Els fitxers han de tenir una extensió vàlida. <em>(e.g. "{0}")</em>',
+  ajaxSuccess: '<strong>{0}</strong> no està disponible.',
+  ajaxError: 'Error en el servidor...'
+
+})
